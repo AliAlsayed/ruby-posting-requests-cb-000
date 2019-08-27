5bf69d79a2d0a19b36008ceacd4b2dfc8e07ebaf
Ali Alsayed <a_alsayed87@hotmail.com>
2019-08-27 10:00:32 +0000

    Automatically backed up by Learn

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -4,8 +4,6 @@ class TipsController < ApplicationController
     resp = Faraday.get("https://api.foursquare.com/v2/tips/add") do |req|
       req.params['oauth_token'] = session[:token]
       req.params['v'] = '20160201'
-      req.params['venueId'] = params[:venue_id]
-      req.params['text'] = params[:tip]
     end
   end
   def create
